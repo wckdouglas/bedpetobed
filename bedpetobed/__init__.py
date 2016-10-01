79742d50b9bed3f77e4ce6e9310f70737124c261
wckdouglas <wckdouglas@gmail.com>
2016-10-01 17:09:51 -0500

    commented __init__

--- bedpetobed/__init__.py
+++ bedpetobed/__init__.py
@@ -1,5 +1,5 @@
-import pyximportcpp
-pyximportcpp.install(setup_args={
-            'include_dirs': ['include/']
-        })
-from .filter_bed import *
+#import pyximportcpp
+#pyximportcpp.install(setup_args={
+#            'include_dirs': ['include/']
+#        })
+#from filter_bed import *
